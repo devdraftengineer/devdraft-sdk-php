5ab487db74268f646a838d00338815c13d674122
chomba <uncledevhq@gmail.com>
2025-06-11 04:11:17 +0200

    Initial release: DevDraft PHP SDK

--- git_push.sh
+++ git_push.sh
@@ -7,7 +7,7 @@
 GIT_USER_ID="devraftengineer"
 GIT_REPO_ID="devdraft-sdk-php"
 RELEASE_NOTE="Initial release: DevDraft PHP SDK"
-GIT_REPO_BASE_URL="https://github.com"
+GIT_REPO_BASE_URL="github.com"
 
 # Initialize the local directory as a Git repository
 git init
@@ -24,11 +24,9 @@ git commit -m "$RELEASE_NOTE"
 # Add the new remote
 if [ "$GIT_TOKEN" = "" ]; then
     echo "[INFO] \$GIT_TOKEN (environment variable) is not set. Using the git credential in your environment."
-    git remote add origin "${GIT_REPO_BASE_URL}/${GIT_USER_ID}/${GIT_REPO_ID}.git"
+    git remote add origin "https://${GIT_REPO_BASE_URL}/${GIT_USER_ID}/${GIT_REPO_ID}.git"
 else
-    git_repo_base_url=${GIT_REPO_BASE_URL#*//}
-    git_repo_base_url=${git_repo_base_url%%.*}
-    git remote add origin "https://${GIT_USER_ID}:${GIT_TOKEN}@${git_repo_base_url}/${GIT_USER_ID}/${GIT_REPO_ID}.git"
+    git remote add origin "https://${GIT_USER_ID}:${GIT_TOKEN}@${GIT_REPO_BASE_URL}/${GIT_USER_ID}/${GIT_REPO_ID}.git"
 fi
 
 # Create and switch to main branch
@@ -38,6 +36,6 @@ git branch -M main
 git pull origin main || true
 
 # Pushes the changes in the local repository up to the remote repository
-echo "Git pushing to ${GIT_REPO_BASE_URL}/${GIT_USER_ID}/${GIT_REPO_ID}.git"
+echo "Git pushing to https://${GIT_REPO_BASE_URL}/${GIT_USER_ID}/${GIT_REPO_ID}.git"
 git push -u origin main 2>&1 | grep -v 'To https'
 
