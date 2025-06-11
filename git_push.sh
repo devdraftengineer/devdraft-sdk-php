85f136f19400bea513666d8c0f053d05b9ba23ff
chomba <uncledevhq@gmail.com>
2025-06-11 04:00:23 +0200

    Initial release: DevDraft PHP SDK

--- git_push.sh
+++ git_push.sh
@@ -3,10 +3,11 @@
 #
 # Usage example: /bin/sh ./git_push.sh wing328 swagger-petstore-perl "minor update"
 
-git_user_id="devraftengineer"
-git_repo_id="devdraft-sdk-php"
-release_note="Initial release: DevDraft PHP SDK"
-git_repo_base_url="https://github.com"
+# Set repository details
+GIT_USER_ID="devraftengineer"
+GIT_REPO_ID="devdraft-sdk-php"
+RELEASE_NOTE="Initial release: DevDraft PHP SDK"
+GIT_REPO_BASE_URL="https://github.com"
 
 # Initialize the local directory as a Git repository
 git init
@@ -15,18 +16,18 @@ git init
 git add .
 
 # Commits the tracked changes and prepares them to be pushed to a remote repository.
-git commit -m "$release_note"
+git commit -m "$RELEASE_NOTE"
 
 # Sets the new remote
 git_remote=`git remote`
 if [ "$git_remote" = "" ]; then # git remote not defined
     if [ "$GIT_TOKEN" = "" ]; then
         echo "[INFO] \$GIT_TOKEN (environment variable) is not set. Using the git credential in your environment."
-        git remote add origin ${git_repo_base_url}/${git_user_id}/${git_repo_id}.git
+        git remote add origin "${GIT_REPO_BASE_URL}/${GIT_USER_ID}/${GIT_REPO_ID}.git"
     else
-        git_repo_base_url=${git_repo_base_url#*//}
+        git_repo_base_url=${GIT_REPO_BASE_URL#*//}
         git_repo_base_url=${git_repo_base_url%%.*}
-        git remote add origin https://${git_user_id}:${GIT_TOKEN}@${git_repo_base_url}/${git_user_id}/${git_repo_id}.git
+        git remote add origin "https://${GIT_USER_ID}:${GIT_TOKEN}@${git_repo_base_url}/${GIT_USER_ID}/${GIT_REPO_ID}.git"
     fi
 fi
 
@@ -34,9 +35,9 @@ fi
 git branch -M main
 
 # Pull from main branch
-git pull origin main
+git pull origin main || true
 
 # Pushes the changes in the local repository up to the remote repository
-echo "Git pushing to ${git_repo_base_url}/${git_user_id}/${git_repo_id}.git"
+echo "Git pushing to ${GIT_REPO_BASE_URL}/${GIT_USER_ID}/${GIT_REPO_ID}.git"
 git push -u origin main 2>&1 | grep -v 'To https'
 
